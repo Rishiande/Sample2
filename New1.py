38e2f42fc2ca472697461048087b396bb02c6390
Rishiande <96639486+Rishiande@users.noreply.github.com>
2022-09-05 10:30:33 +0530

    Update New1.py

--- New1.py
+++ New1.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.header('RAMACHANDRA COLLEGE OF ENGINEERING')
+st.header('HAPPY TEACHERS DAY')
 a=st.number_input('ENTER ANY VALUE')
 if st.button('HIT  ME'):
   st.success(f'YOUR LUCKY NMBER IS {a}')
